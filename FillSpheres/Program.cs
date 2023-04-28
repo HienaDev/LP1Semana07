0851dd3f8c604f04da77e7dd6477c447207a2d7a
HienaDev <ant.rodrigues1999@gmail.com>
2023-04-28 14:15:14 +0100

    Added testing to main

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -87,7 +87,21 @@ namespace FillSpheres
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Color colorRed = new Color (255, 0, 0);
+            Color colorGreen = new Color (0, 255, 0);
+
+            Sphere redBall = new Sphere(colorRed, 5f);
+            Sphere greenBall = new Sphere(colorGreen, 2f);
+
+            Console.WriteLine($"Red Ball was thrown {redBall.GetTimesThrown()} times with color ({colorRed.GetRed()}, {colorRed.GetGreen()}, {colorRed.GetBlue()}, {colorRed.GetAlpha()})");
+            redBall.Throw();
+            Console.WriteLine($"Red Ball was thrown {redBall.GetTimesThrown()} times with color ({colorRed.GetRed()}, {colorRed.GetGreen()}, {colorRed.GetBlue()}, {colorRed.GetAlpha()})");
+            Console.WriteLine($"Green Ball was thrown {greenBall.GetTimesThrown()} times with color ({colorGreen.GetRed()}, {colorGreen.GetGreen()}, {colorGreen.GetBlue()}, {colorGreen.GetAlpha()})");
+            greenBall.Throw();
+            Console.WriteLine($"Green Ball was thrown {greenBall.GetTimesThrown()} times with color ({colorGreen.GetRed()}, {colorGreen.GetGreen()}, {colorGreen.GetBlue()}, {colorGreen.GetAlpha()})");
+            greenBall.Pop();
+            greenBall.Throw();
+            Console.WriteLine($"Green Ball was thrown {greenBall.GetTimesThrown()} times with color ({colorGreen.GetRed()}, {colorGreen.GetGreen()}, {colorGreen.GetBlue()}, {colorGreen.GetAlpha()})");
         }
     }
 }
