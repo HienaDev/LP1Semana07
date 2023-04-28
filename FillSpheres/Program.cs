9ed8af928aa70fdae07c062e235619e98d015deb
HienaDev <ant.rodrigues1999@gmail.com>
2023-04-28 13:50:28 +0100

    Added class sphere and constructor

---
+++ FillSpheres/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FillSpheres
+{
+    public class Color
+    {
+        private byte red;
+        private byte green;
+        private byte blue;
+        private byte alpha;
+
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
+
+        public Color(byte red, byte green, byte blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = 255;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
